39a237cc7eef0d23104089b7afa6c0085b71c640
Alexandre Bacellar <alexandre.bacellar@gmail.com>
2024-07-04 16:40:44 -0300

    first commit

---
+++ src/main/java/io/github/bacellar08/btgpactual/msorder/MsorderApplication.java
@@ -0,0 +1,13 @@
+package io.github.bacellar08.btgpactual.msorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsorderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/bacellar08/btgpactual/msorder/MsorderApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bacellar08.btgpactual.msorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
